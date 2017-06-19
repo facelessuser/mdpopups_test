9077d45703315e6db2b13409da2a6def4c3134a4
facelessuser <faceless.shop@gmail.com>
2017-06-19 17:27:03 -0600

    Cleanup and update readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# mdpopup_test
+# MdPopups Test Plugin
 
 ![img](tooltips_test.png)
+
+Run command `Mdpopups: test` and choose either popup or phantom.
+
+Edit `test.py` to play around with the commands.
 
 Need Mdpopups 2.1.0+
--- test.py
+++ test.py
@@ -19,9 +19,10 @@ If you need to clear the cache.
 import sublime
 import sublime_plugin
 import mdpopups
-from textwrap import dedent
 import sys
 this = sys.modules[__name__]
+
+TEST_MD = "Packages/mdpopup_test/test.md"
 
 frontmatter = {
     "markdown_extensions": [
@@ -45,191 +46,6 @@ frontmatter = {
         {"pymdownx.smartsymbols": {"ordinal_numbers": False}}
     ]
 }
-
-md_text = dedent(
-    '''\
-    # Test Output
-    ## Headers
-
-    # H1
-    ## H2
-    ### H3
-    #### H4
-    ##### H5
-    ###### H6
-
-    ## Horizontal Ruler
-
-    ---
-
-    ## Paragraphs
-
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat...
-
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-
-    ## Inline
-
-    ++ctrl+alt+del++
-
-    Inline **bold**.
-
-    Inline *italic*.
-
-    Inline `code`.
-
-    Inline `#!python import code`
-
-    [mdpopups link](https://github.com/facelessuser/sublime-markdown-popups)
-
-    https://github.com/facelessuser/sublime-markdown-popups/issues/32
-
-    https://github.com/facelessuser/sublime-markdown-popups/commit/ffa39c2ea6a92752eaaab103e4456bdc39918978
-
-    https://github.com/facelessuser/pymdown-extensions/commit/152e85baa10616054535f4bfeb99345d87741655
-
-    ## SmartSymbols
-
-    `(tm)`: (tm)
-
-    `(c)`: (c)
-
-    `(r)`: (r)
-
-    `c/o`: c/o
-
-    `+/-`: +/-
-
-    `-->`: -->
-
-    `<--`: <--
-
-    `<-->`: <-->
-
-    `=/=`: =/=
-
-    `1/4, etc.`: 1/4, etc.
-
-    ## Quotes
-
-    > Here is a quote.  
-    > About something.
-
-    ## Definition Lists
-
-    Apple
-    : 1) The round fruit of a tree of the rose family, which typically has thin red or green 
-        skin and crisp flesh.
-
-        Many varieties have been developed as dessert or cooking fruit or for making cider.
-
-    : 2) The tree which bears apples.
-
-    ## Normal Lists
-
-    - Fruit
-        - Apples
-        - Bannanas
-        - Oranges
-        - Grapes
-
-    1. Meat
-        1. Chicken
-        2. Pork
-        3. Beef
-
-    1. Lists With Paragraphs
-        1. item 1
-
-            More from Item 1.
-
-        2. item 2
-
-        3. item 3
-
-    ## Blocks
-
-        Indented code block
-        goes here
-
-    ```python
-    # Fenced code block
-    import awesome
-    ```
-
-    - Nested Fence:
-
-        ```python
-        # Fenced code block
-        import awesome
-        ```
-
-    ## Admonition
-
-    !!! panel-other "Normal"
-        Testing admontions.
-
-        ```python
-        import test
-
-        string = """Lorem  ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim 
-        laboris nisi ut aliquip ex ea commodo consequat..."""
-        ```
-
-    !!! panel-success "Success"
-        Testing admontions.
-
-        ```python
-        import test
-
-        string = """Lorem  ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim 
-        laboris nisi ut aliquip ex ea commodo consequat..."""
-        ```
-
-    !!! panel-warning "Warning"
-        Testing admontions.
-
-        ```python
-        import test
-
-        string = """Lorem  ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim 
-        laboris nisi ut aliquip ex ea commodo consequat..."""
-        ```
-
-    !!! panel-error "Error"
-        Testing admontions.
-
-        ```python
-        import test
-
-        string = """Lorem  ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim 
-        laboris nisi ut aliquip ex ea commodo consequat..."""
-        ```
-
-    !!! panel-info "Info"
-        Testing admontions.
-
-        ```python
-        import test
-
-        string = """Lorem  ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim 
-        laboris nisi ut aliquip ex ea commodo consequat..."""
-        ```
-    '''
-)
 
 
 def active_view():
@@ -299,13 +115,13 @@ def show_phantom(text):
 def mdpopups_popup_format_test():
     """Test popup."""
 
-    show_popup(mdpopups.format_frontmatter(frontmatter) + md_text)
+    show_popup(mdpopups.format_frontmatter(frontmatter) + sublime.load_resource(TEST_MD))
 
 
 def mdpopups_phantom_format_test():
     """Test phantom."""
 
-    show_phantom(mdpopups.format_frontmatter(frontmatter) + md_text)
+    show_phantom(mdpopups.format_frontmatter(frontmatter) + sublime.load_resource(TEST_MD))
 
 
 class MdpopupsTestCommand(sublime_plugin.TextCommand):
